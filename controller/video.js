3a2b5fad7eb390f443ead7989c4df94be4879a50
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2024-06-11 02:40:45 +0530

    checking trending

--- controller/video.js
+++ controller/video.js
@@ -79,8 +79,8 @@ export const addView = async(req, res) => {
 
 export const trending = async (req, res) => {
     try{
-        const videos = await DYvideo.find().sort({views: -1});
-        return res.status(200).json(videos);
+        // const videos = await DYvideo.find().sort({views: -1});
+        return res.send("sup dude");
     }
     catch(err){
         return res.status(500).json("error in fetching trending");
